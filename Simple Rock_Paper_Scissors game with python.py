5ab3b1b113b27b35f71d97f8d78e2da554a7dcd0
Drax_10017 <86570298+Ranger-DRAX@users.noreply.github.com>
2023-10-11 04:59:35 +0600

    Simple Rock_Paper_Scissors game with python.py

--- Simple Rock_Paper_Scissors game with python.py
+++ Simple Rock_Paper_Scissors game with python.py
@@ -12,3 +12,11 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
 print(play())
+'''
+██████╗ ██████╗  █████╗ ██╗  ██╗
+██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝
+██║  ██║██████╔╝███████║ ╚███╔╝ 
+██║  ██║██╔══██╗██╔══██║ ██╔██╗ 
+██████╔╝██║  ██║██║  ██║██╔╝ ██╗
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                                '''
